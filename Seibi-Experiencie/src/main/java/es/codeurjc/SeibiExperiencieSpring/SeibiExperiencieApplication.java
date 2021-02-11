e2cbbbd01d8e7a6b50ac0bdad147f45a6202592b
AruxG <48557378+AruxG@users.noreply.github.com>
2021-02-11 11:26:57 +0100

    Spring Project Created
    Creación del proyecto inicial en Spring Boot

---
+++ Seibi-Experiencie/src/main/java/es/codeurjc/SeibiExperiencieSpring/SeibiExperiencieApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.SeibiExperiencieSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeibiExperiencieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeibiExperiencieApplication.class, args);
+	}
+
+}
---
+++ Seibi-Experiencie/src/test/java/es/codeurjc/SeibiExperiencieSpring/SeibiExperiencieApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.SeibiExperiencieSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeibiExperiencieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
