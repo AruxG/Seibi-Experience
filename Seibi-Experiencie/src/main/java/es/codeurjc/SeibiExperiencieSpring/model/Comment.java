c6f30edac40df65ea418cdd9e3358e2668a7d93d
rociiocs <rociiocs.00@gmail.com>
2021-03-04 12:43:21 +0100

    Revert "Revert "CambioH2-Mysql""
    This reverts commit 8c9b2ee3adf624ee29732de9ac6c8d84b47b7240.

--- Seibi-Experiencie/src/main/java/es/codeurjc/SeibiExperiencieSpring/model/Comment.java
+++ Seibi-Experiencie/src/main/java/es/codeurjc/SeibiExperiencieSpring/model/Comment.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class Comment {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
 	@ManyToOne
