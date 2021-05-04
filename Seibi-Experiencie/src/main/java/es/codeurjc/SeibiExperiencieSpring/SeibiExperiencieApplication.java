4e47f419de8a7d22258c3e587046a81adb73af31
rociiocs <49315467+rociiocs@users.noreply.github.com>
2021-05-04 15:15:40 +0200

    Hazelcast
    Implementado hazelcast para que ambas aplicaciones se reconozcan. A falta de que se realice correctamente en docker.
    
    Co-Authored-By: AruxG <48557378+AruxG@users.noreply.github.com>

--- Seibi-Experiencie/src/main/java/es/codeurjc/SeibiExperiencieSpring/SeibiExperiencieApplication.java
+++ Seibi-Experiencie/src/main/java/es/codeurjc/SeibiExperiencieSpring/SeibiExperiencieApplication.java
@@ -5,11 +5,13 @@ import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class SeibiExperiencieApplication {
 
 	public static void main(String[] args) {
